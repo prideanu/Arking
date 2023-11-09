5e6b1fe5bbf880dee688c9a1ef3f1c1e6c033e1d
prideanu <132413677+prideanu@users.noreply.github.com>
2023-11-09 19:36:50 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6005960715:AAHB9INP_OzN_b4gZzKeRMOM--X8H6wfkAc"
+api_id = 27526569
+api_hash = "1b7c2d98893388c9917cd75855d0a5a6"
+bot_token = "6796858401:AAEKDtOlsSNoUTDa-qTXw7o-dhi-KQX6X-M"
 
